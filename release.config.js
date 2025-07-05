a6da22fc5334e2b079cf8901b43dd58e4dcb50b2
YU <111847207+YU000jp@users.noreply.github.com>
2025-07-06 08:07:31 +0900

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -6,6 +6,10 @@ module.exports = {
     plugins: [
         ['@semantic-release/commit-analyzer', {
             preset: 'conventionalcommits',
+            releaseRules: [
+                { type: 'ref', release: 'patch' },
+                { type: 'chore', release: 'patch' },
+            ],
         }],
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
